68e8c20e1afb35e0ddf91581f334c0c653290d53
EricReinhart <hr.vitaly@mail.ru>
2023-06-08 20:48:51 +0300

    small changes with docker

--- dockerfile
+++ dockerfile
@@ -8,5 +8,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . /code/
 ENV DJANGO_SETTINGS_MODULE=OwlNest.settings
 ENV PYTHONUNBUFFERED=1
-COPY gunicorn.conf /code/
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "OwlNest.wsgi:application", "-c", "/code/gunicorn.conf"]
+COPY gunicorn.conf /code/ 
+CMD ["/code/release.sh"]
--- release.sh
+++ release.sh
@@ -3,5 +3,4 @@ set -e
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-echo "from nest.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell 
-gunicorn OwlNest.wsgi:application -c /code/gunicorn.conf
+exec gunicorn --bind 0.0.0.0:8000 OwlNest.wsgi:application -c /code/gunicorn.conf
