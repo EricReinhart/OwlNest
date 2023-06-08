51b23bf168aca5a2203f46bbb8d893f212e78958
EricReinhart <hr.vitaly@mail.ru>
2023-06-08 19:50:11 +0300

    docker

--- release.sh
+++ release.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+echo "from nest.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell 
 gunicorn OwlNest.wsgi:application -c /code/gunicorn.conf
